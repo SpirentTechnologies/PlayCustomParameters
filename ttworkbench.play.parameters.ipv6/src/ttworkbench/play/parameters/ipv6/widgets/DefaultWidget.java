2bc101d63cf534f9e0454777f0693446438dbdd6
a-d <duemont@mi.fu-berlin.de>
2014-02-25 14:45:12 +0100

    added table viewer widget, kept grid view as type

--- ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/widgets/DefaultWidget.java
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/widgets/DefaultWidget.java
@@ -1,83 +1,67 @@
 package ttworkbench.play.parameters.ipv6.widgets;
 
-import java.awt.Label;
 import java.util.Set;
-
-import org.eclipse.jface.viewers.IContentProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.List;
 
-import com.testingtech.ttworkbench.ttman.parameters.api.IAttribute;
+import ttworkbench.play.parameters.ipv6.widgets.tableviewer.WidgetTableViewerControl;
+import ttworkbench.play.parameters.ipv6.widgets.tableviewer.TableParameterColumnType;
+
 import com.testingtech.ttworkbench.ttman.parameters.api.IParameter;
-import com.testingtech.ttworkbench.ttman.parameters.api.IParameterEditor;
-import com.testingtech.ttworkbench.ttman.parameters.api.IParameterValidator;
-import com.testingtech.ttworkbench.ttman.parameters.api.IParameterValueProvider;
-import com.testingtech.ttworkbench.ttman.parameters.api.IWidget;
 
 public class DefaultWidget extends AbstractWidget {
 
 	private static final String TITLE = "Default Widget";
 	private static final String DESCRIPTION = "";
 	private static final Image IMAGE = null;
-	
+
 	public DefaultWidget() {
 		super( TITLE, DESCRIPTION, IMAGE);
 	}
-	
+
 	@Override
 	public Control createControl(Composite theParent) {
-		
-		theParent.setLayout( new FillLayout(SWT.HORIZONTAL));
-		
+
+		theParent.setLayout( new FillLayout( SWT.HORIZONTAL));
+
 		ScrolledComposite scrolledComposite = new ScrolledComposite( theParent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
 		scrolledComposite.setExpandHorizontal( true);
 		scrolledComposite.setExpandVertical( true);
-		
-		
-		Composite editorContainer = new Composite( scrolledComposite, SWT.None);
-		
-		/*
-		 * TODO replace this later with the TableViewer component
-		 * final TableViewer tableViewer = new TableViewer( editorContainer);
-		 */
-		
-		GridData gridData = new GridData( SWT.FILL, SWT.FILL, true, false);
-		editorContainer.setLayout( new GridLayout( 2, false));
-	    Set<IParameterEditor> editors = getEditors();
-	    for ( IParameterEditor editor : editors) {
-			
-		    CLabel label = new CLabel( editorContainer, SWT.LEFT);
-			label.setText( editor.getParameter().getName());
-			label.setLayoutData( gridData);
-			
-			label = new CLabel( editorContainer, SWT.LEFT);
-			label.setText( editor.getParameter().getValue().toString());
-			label.setLayoutData( gridData);
-			
-		}
 
-		scrolledComposite.setContent( editorContainer);
-		scrolledComposite.setMinSize( editorContainer.computeSize( SWT.DEFAULT, SWT.DEFAULT));
-				// TODO Auto-generated method stub
+		/* deprecated
+		Composite composite = new WidgetGrid( scrolledComposite)
+			.setEditors( getEditors())
+			.getComposite();
+		*/
+		
+		Composite composite = new WidgetTableViewerControl(scrolledComposite)
+		
+			.addParamterEditorHolders("All parameters", getEditors())
+			
+			.addTableColumn(TableParameterColumnType.COLUMN_PARAMETER_NAME, "Parameter Name")
+			.addTableColumn(TableParameterColumnType.COLUMN_PARAMETER_VALUE, "Value")
+			.addTableColumn(TableParameterColumnType.COLUMN_PARAMETER_DEFAULT, "Default")
+			.addTableColumn(TableParameterColumnType.COLUMN_PARAMETER_TYPE, "Type")
+			.addTableColumn(TableParameterColumnType.COLUMN_PARAMETER_DESCRIPTION, "Description")
+			
+			.getComposite();
+		
+
+		scrolledComposite.setContent( composite);
+		scrolledComposite.setMinSize( composite.computeSize( SWT.DEFAULT, SWT.DEFAULT));
 		return scrolledComposite;
 	}
 
+	
+
 	@Override
-	protected Set<IParameter> filterRelevantParameters(
-			Set<IParameter> theParameters) {
+	protected Set<IParameter> filterRelevantParameters(Set<IParameter> theParameters) {
 		return theParameters;
 	}
+
 
 }
---
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/widgets/tableviewer/WidgetTableViewerControl.java
@@ -0,0 +1,147 @@
+package ttworkbench.play.parameters.ipv6.widgets.tableviewer;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+
+import com.testingtech.ttworkbench.ttman.parameters.api.IParameterEditor;
+
+public class WidgetTableViewerControl {
+
+
+	private class ColumnDefinition {
+		public TableParameterColumnType columnType;
+		public String name;
+		public int direction;
+		public ColumnDefinition(TableParameterColumnType columnType, String name, int direction) {
+			this.columnType = columnType;
+			this.name = name;
+			this.direction = direction;
+		}
+	}
+	
+	private Composite composite;
+	private Composite scrolledComposite;
+	private Combo combo;
+	private TableViewer tv;
+	private TableViewerLabelProvider parameterLabelProvider = new TableViewerLabelProvider();
+	private LinkedList<ParameterEditorHolder> holders = new LinkedList<ParameterEditorHolder>();
+	private LinkedList<ColumnDefinition> columnDefinitions = new LinkedList<WidgetTableViewerControl.ColumnDefinition>();
+
+	
+	public WidgetTableViewerControl(Composite parent) {
+		this.scrolledComposite = parent;
+	}
+
+	public Composite getComposite() {
+		
+		if(composite==null) {
+			createComposite();
+		}
+		
+		return composite;
+	}
+
+	private void createComposite() {
+		composite = new Composite( scrolledComposite, SWT.NONE);
+		composite.setLayout( new GridLayout( 1, false));
+
+		// Create the combo to hold the team names
+		combo = new Combo( composite, SWT.READ_ONLY);
+		combo.setLayoutData( new GridData( GridData.FILL_HORIZONTAL));
+
+		// Set the content and label providers
+		tv = new TableViewer( composite, SWT.None);
+		
+		tv.setContentProvider( new TableViewerContentProvider());
+		tv.setLabelProvider( parameterLabelProvider);
+		tv.setSorter( new TableViewerSorter());
+		
+		configureTableColumns();
+		configureTableContents();
+	}
+
+	
+
+	private void configureTableColumns() {
+		for(final ColumnDefinition coldef : columnDefinitions) {
+			TableColumn tc = new TableColumn( tv.getTable(), coldef.direction);
+			tc.setText( coldef.name);
+			tc.addSelectionListener( new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent event) {
+					( (TableViewerSorter) tv.getSorter()).doSort( coldef.columnType);
+					tv.refresh();
+				}
+			});
+			parameterLabelProvider.addColumn( coldef.columnType);
+		}
+	}
+
+
+	
+	public WidgetTableViewerControl addTableColumn(TableParameterColumnType columnType, String name) {
+		addTableColumn(columnType, name, SWT.LEFT);
+		return this;
+	}
+	
+	public WidgetTableViewerControl addTableColumn(TableParameterColumnType columnType, String name, int direction) {
+		columnDefinitions.add( new ColumnDefinition(columnType, name, direction));
+		return this;
+	}
+
+	
+	public WidgetTableViewerControl addParamterEditorHolders(String label, Collection<IParameterEditor> editors) {
+		holders.add( new ParameterEditorHolder(label, editors));
+		return this;
+	}
+	
+	
+	private void configureTableContents() {
+		for (ParameterEditorHolder editor : holders) {
+			combo.add(editor.getLabel());
+		}
+
+		// Add a listener to change the tableviewer's input
+		combo.addSelectionListener( new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				update(holders.get( ((Combo) event.widget).getSelectionIndex()));
+			}
+		});
+
+		// Select the first item
+		combo.select(0);
+		update(holders.getFirst());
+
+		// Pack the columns
+		Table table = tv.getTable();
+		for(TableColumn tc : table.getColumns()) {
+			tc.pack();
+		}
+
+		// Turn on the header and the lines
+		table.setLayoutData( new GridData( GridData.FILL_BOTH));		
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+	}
+
+	/**
+	 * Updates the application with the selected team
+	 * 
+	 * @param team
+	 *            the team
+	 */
+	private void update(ParameterEditorHolder parameterEditorHolder) {
+		tv.setInput(parameterEditorHolder);
+	}
+
+}
