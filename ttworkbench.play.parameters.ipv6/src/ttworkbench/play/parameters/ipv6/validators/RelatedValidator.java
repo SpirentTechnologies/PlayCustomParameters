c784986377984fd230f8adac85f2a1e7a8d0b5ce
Johannes Dahlke <johannes.dahlke@fu-berlin.de>
2014-03-19 13:57:28 +0100

    add context attribute to validators

--- ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/validators/RelatedValidator.java
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/validators/RelatedValidator.java
@@ -4,19 +4,25 @@ import java.util.HashMap;
 
 import com.testingtech.ttworkbench.ttman.parameters.api.IParameter;
 import com.testingtech.ttworkbench.ttman.parameters.api.IParameterEditor;
+import com.testingtech.ttworkbench.ttman.parameters.api.IValidatorContext;
 
 public abstract class RelatedValidator extends AbstractValidator {
+	
+	
 	public interface RelationKey {
 		
 	}
+	private IValidatorContext context;
+	
 	
 	public abstract RelationKey[] getRelationKeys();
 	
 	private HashMap<RelationKey, IParameter<?>> relatedParameters = new HashMap<RelationKey, IParameter<?>>();	
 	private HashMap<RelationKey, IParameterEditor<?>> relatedEditorsInWidget = new HashMap<RelationKey, IParameterEditor<?>>();
 
-	public RelatedValidator( String theTitle, String theDescription) {
+	public RelatedValidator( String theTitle, String theDescription, IValidatorContext theContext) {
 		super( theTitle, theDescription);
+		this.context = theContext;
 	}
 
 	public void addParameter( RelationKey key, IParameter<?> parameter) {
@@ -36,5 +42,21 @@ public abstract class RelatedValidator extends AbstractValidator {
 	public <T> IParameterEditor<T> getEditor( RelationKey key) {
 		return (IParameterEditor<T>) relatedEditorsInWidget.get(key);
 	}
+	
+	protected IValidatorContext getContext() {
+		return context;
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO + context 
+		return super.hashCode() + 31*context.hashCode();
+	}
+	
+	@Override
+	public long getId() {
+		return hashCode();
+	}
+  
 
 }
