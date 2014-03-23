ff92da60b78d08bdeffd7e52c615cf71c5288c33
Thomas Büttner <thomas.buettner@fu-berlin.de>
2014-03-23 16:37:59 +0100

    MacWidget-Composer : Repair Validator

--- ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/composer/MacWidgetComposer.java
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/composer/MacWidgetComposer.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ttworkbench.play.parameters.ipv6.ParameterMap;
-import ttworkbench.play.parameters.ipv6.editors.MacAddressEditor;
 import ttworkbench.play.parameters.ipv6.editors.ValidatingEditor;
+import ttworkbench.play.parameters.ipv6.editors.macaddr.MacAddressEditor;
 import ttworkbench.play.parameters.ipv6.validators.AbstractValidator;
 import ttworkbench.play.parameters.ipv6.widgets.MacWidget;
 
@@ -17,61 +17,62 @@ import com.testingtech.ttworkbench.ttman.parameters.api.IWidget;
 import com.testingtech.ttworkbench.ttman.parameters.validation.ErrorKind;
 import com.testingtech.ttworkbench.ttman.parameters.validation.ValidationResult;
 
-public class MacWidgetComposer extends WidgetComposer{
-	
+public class MacWidgetComposer extends WidgetComposer {
+
 	// get relevant parameters
 	final IParameter<String> parameter_MacAddress = getParametersMap().getParameterById( "PC_MAC_UCA_HS01");
-	
+
 	public MacWidgetComposer( IConfigurator theConfigurator, ParameterMap theParametersMap) {
 		super( theConfigurator, theParametersMap);
 		// TODO Auto-generated constructor stub
-}
+	}
 
 	@Override
-	public void compose(){
-		//declare a Mac Address Widget
+	public void compose() {
+		// declare a Mac Address Widget
 		IWidget macWidget = new MacWidget();
-		//add the Mac widget to the frame work
+		// add the Mac widget to the frame work
 		getConfigurator().addWidget( macWidget);
-		
+
 		ValidatingEditor<?> editor_MacAddress = new MacAddressEditor();
-		
-		//The MAC validator
-		IParameterValidator macValidator = new AbstractValidator( "MAC Address Validator", ""){
+
+		// The MAC validator
+		IParameterValidator macValidator = new AbstractValidator( "MAC Address Validator", "") {
 			@Override
-			protected List<ValidationResult> validateParameter( IParameter parameter) {
+			protected List<ValidationResult> validateParameter(IParameter parameter, Object theClient) {
 				List<ValidationResult> validationResults = new ArrayList<ValidationResult>();
 
-				String theValue = ((StringValue)parameter.getValue()).getTheContent();
-				System.out.println("this is my parameter value:  "+theValue);
-				if ( isMacAddress( theValue))
-					validationResults.add( new ValidationResult("This entry has a valid MAC Address format.", ErrorKind.success, "tag_is_mac"));
+				String theValue = ( (StringValue) parameter.getValue()).getTheContent();
+				System.out.println( "this is my parameter value:  " + theValue);
+				if (isMacAddress( theValue))
+					validationResults.add( new ValidationResult( "This entry has a valid MAC Address format.", ErrorKind.success,
+							"tag_is_mac"));
 				else {
-					validationResults.add( new ValidationResult( "This entry does not have a valid MAC Address format.", ErrorKind.error, "tag_is_mac"));
+					validationResults.add( new ValidationResult( "This entry does not have a valid MAC Address format.",
+							ErrorKind.error, "tag_is_mac"));
 				}
 				return validationResults;
 			}
 
 		};
-		
+
 		// assign each parameter to the corresponding editor in this widget
 		getConfigurator().assign( editor_MacAddress, macWidget, parameter_MacAddress);
-		
-		//Register the mac validator to the editor
+
+		// Register the mac validator to the editor
 		macValidator.registerForMessages( editor_MacAddress);
-		
-		//assign the validator to the parameter
+
+		// assign the validator to the parameter
 		getConfigurator().assign( macValidator, macWidget, parameter_MacAddress);
-		
+
 	}
-  
 
 	// Chechk if the entered Mac Address has a valid format
-	private boolean isMacAddress(String macEntry){
+	private boolean isMacAddress(String macEntry) {
 		final String MAC_PATTERN1 = "^([0-9a-fA-F]{2}[:-]){5}([0-9a-fA-F]{2})$";
 		final String MAC_PATTERN2 = "^([0-9a-fA-F]{2}[-]){5}([0-9a-fA-F]{2})$";
 		final String MAC_PATTERN3 = "^([0-9a-fA-F]{12})$";
-		if (macEntry.matches( MAC_PATTERN1) || macEntry.matches( MAC_PATTERN2) || macEntry.matches( MAC_PATTERN3)){
+		if (macEntry.matches( MAC_PATTERN1) || macEntry.matches( MAC_PATTERN2) || macEntry.matches( MAC_PATTERN3)) {
 			return true;
 		} else {
 			return false;
