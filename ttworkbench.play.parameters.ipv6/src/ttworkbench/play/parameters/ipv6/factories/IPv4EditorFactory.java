a836d86d5f891bf7a89520479f7c859bfc694259
Thomas Büttner <thomas.buettner@fu-berlin.de>
2014-03-24 13:13:05 +0100

    Fixed EditorFactorys for IPEditors

--- ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/factories/IPv4EditorFactory.java
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/factories/IPv4EditorFactory.java
@@ -2,10 +2,10 @@ package ttworkbench.play.parameters.ipv6.factories;
 
 import java.util.List;
 
-import org.eclipse.swt.widgets.Composite;
+import ttworkbench.play.parameters.ipv6.editors.ip.IPEditor;
+import ttworkbench.play.parameters.ipv6.editors.ip.IPv4Verifier;
 
 import com.testingtech.ttworkbench.ttman.parameters.api.IAttribute;
-import com.testingtech.ttworkbench.ttman.parameters.api.IConfiguration;
 import com.testingtech.ttworkbench.ttman.parameters.api.IParameter;
 import com.testingtech.ttworkbench.ttman.parameters.api.IParameterEditor;
 import com.testingtech.ttworkbench.ttman.parameters.api.IParameterEditorFactory;
@@ -19,8 +19,8 @@ public class IPv4EditorFactory implements IParameterEditorFactory {
 	@Override
 	public boolean isApplicable(IParameter<? extends Object> parameter) {
 		// TODO Auto-generated method stub
-		parameter.getType(); //"Module1.Type2"
-		parameter.getName(); //"Module1.Parameter2"
+		parameter.getType(); // "Module1.Type2"
+		parameter.getName(); // "Module1.Parameter2"
 		return false;
 	}
 
@@ -55,91 +55,9 @@ public class IPv4EditorFactory implements IParameterEditorFactory {
 
 	@Override
 	public IParameterEditor create() {
-		
-		return new IParameterEditor<Integer>() {
 
-			private static final String TITLE = "IPv4 Parameter Editor";
-			
-			@Override
-			public void setAttribute(String theName, String theValue) {
-				// TODO Auto-generated method stub
-				
-			}
+		return new IPEditor( new IPv4Verifier());
 
-			@Override
-			public void parametersChanged(List<IParameter<?>> theParameters) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public String getTitle() {
-				return TITLE;
-			}
-
-			@Override
-			public String getDescription() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public void setEnabled(boolean theEnable) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public boolean isEnabled() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public void setVisible(boolean theVisible) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public boolean isVisible() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public void setParameter(IParameter<Integer> theParameter) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public IParameter<Integer> getParameter() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public void setConfiguration(IConfiguration theTheConfiguration) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void updateConfig() {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public Composite createControl(Composite theParent) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-		};
-				
 	}
 
 }
-
