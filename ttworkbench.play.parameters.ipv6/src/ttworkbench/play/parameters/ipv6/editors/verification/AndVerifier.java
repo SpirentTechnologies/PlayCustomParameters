0d6c99059bfa296e0f68acc40a7175d18b3477bd
Thomas Büttner <thomas.buettner@fu-berlin.de>
2014-03-24 14:41:06 +0100

    Add AndVerifier, Minor Changes at OrVerifier

--- ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/editors/verification/AndVerifier.java
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/editors/verification/AndVerifier.java
@@ -10,11 +10,10 @@ import ttworkbench.play.parameters.ipv6.components.messaging.data.MessageRecord;
 
 import com.testingtech.ttworkbench.ttman.parameters.validation.ErrorKind;
 
-public class OrVerifier implements IVerifier<String> {
-
+public class AndVerifier implements IVerifier<String> {
 	Set<IVerifier<String>> verifier;
 
-	public OrVerifier( final IVerifier<String>... verifier) {
+	public AndVerifier( final IVerifier<String>... verifier) {
 		this.verifier = new HashSet<IVerifier<String>>( Arrays.asList( verifier));
 	}
 
@@ -22,23 +21,22 @@ public class OrVerifier implements IVerifier<String> {
 	public VerificationResult<String> verify(String theInput, Object... theParams) {
 		List<MessageRecord> messages = new LinkedList<MessageRecord>();
 		boolean verified = false;
-		IVerifier<String> validVerifier = null;
+		IVerifier<String> notValidVerifier = null;
 
 		for (IVerifier<String> v : verifier) {
 			VerificationResult<String> result = v.verify( theInput, theParams);
-			verified |= result.verified;
-			if (verified) {
-				validVerifier = v;
+			verified &= result.verified;
+			if (!verified) {
+				notValidVerifier = v;
 				break;
 			}
 		}
 
 		if (verified) {
-			messages = Arrays
-					.asList( new MessageRecord( "validInput", "valid " + validVerifier.toString(), ErrorKind.success));
+			messages = Arrays.asList( new MessageRecord( "validInput", "valid " + this.toString(), ErrorKind.success));
 		} else {
-			messages = Arrays
-					.asList( new MessageRecord( "validInput", "is not a valid " + this.toString(), ErrorKind.warning));
+			messages = Arrays.asList( new MessageRecord( "validInput", "is not a valid " + notValidVerifier.toString(),
+					ErrorKind.warning));
 		}
 		return new VerificationResult<String>( theInput, verified, messages);
 	}
@@ -48,7 +46,7 @@ public class OrVerifier implements IVerifier<String> {
 		boolean first = true;
 		for (IVerifier<String> v : verifier) {
 			if (!first) {
-				buffer.append( " or ");
+				buffer.append( " and ");
 			}
 			buffer.append( v.toString());
 			first = false;
