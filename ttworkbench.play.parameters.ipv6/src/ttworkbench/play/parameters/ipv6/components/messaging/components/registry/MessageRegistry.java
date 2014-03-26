a0134d7df1f72b1c0dfbdabf9d9787b49fc9da40
Johannes Dahlke <johannes.dahlke@fu-berlin.de>
2014-03-26 11:39:18 +0100

    insert error and warning icons in tab

--- ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/components/messaging/components/registry/MessageRegistry.java
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/components/messaging/components/registry/MessageRegistry.java
@@ -99,7 +99,7 @@ public class MessageRegistry implements IMessageRegistry, IMessageInformation {
 			
 		  // we inform all listeners in parent about this new registration 
 			for ( IRegistryListener listener : parentRegistry.listeners) {
-				listener.handleHydraPublishedEvent( theMessageHydra);
+				listener.handleHydraPublishedEvent( this, theMessageHydra);
 			}
 		}
 	}
@@ -136,6 +136,11 @@ public class MessageRegistry implements IMessageRegistry, IMessageInformation {
 	  	parentRegistry.retrieveHydra( theMessageHydra);
 		errorKindMap.get( theMessageHydra.getErrorKind()).remove( theMessageHydra);
 		messageMap.values().removeAll( Collections.singleton( theMessageHydra));
+	
+		// we inform all listeners in parent about this cancellation 
+		for ( IRegistryListener listener : parentRegistry.listeners) {
+			listener.handleRetrievePublishedEvent( this, theMessageHydra);
+		}
 	}
 	
 
