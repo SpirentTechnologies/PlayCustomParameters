c13d680a572420fed153b5d37489cc69cc48ca2f
fellaws <fellaws@fellaws-machine>
2014-03-27 18:31:10 +0100

    Added Copyright

--- ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/components/messaging/controls/MessageChangeListener.java
+++ ttworkbench.play.parameters.ipv6/src/ttworkbench/play/parameters/ipv6/components/messaging/controls/MessageChangeListener.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * Copyright (c)  .
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * You may not use this file except in compliance with the License.
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * This project came to life under the cooperation of the Authors (cited below) and the Testing Technologies GmbH company in the frame of a University Project proposed by the FU-Berlin.
+ * 
+ * The software is basically a plug-in for the company's eclipse-based framework TTWorkbench. The plug-in offers a new user-friendly view that enables easy configuration of parameters meant to test IPv6 environments.
+ * 
+ * 
+ * Contributors:
+ *     
+ ******************************************************************************/
 package ttworkbench.play.parameters.ipv6.components.messaging.controls;
 
 public interface MessageChangeListener {
